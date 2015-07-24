b134038cb39136380deedb2b3aff186562911917
ryantaylor <ryan.taylor9@gmail.com>
2015-07-24 17:37:20 -0400

    Formatting change to node files to set indents to two spaces instead of 4.

--- node/server.js
+++ node/server.js
@@ -12,30 +12,30 @@ var app = express();
 app.use( bodyParser.json() );
 
 app.post( '/webhooks/rtaylordev', function ( req, res ) {
-	try {
-		var hmac = crypto.createHmac( 'sha1', process.env.WHSECRET_RTAYLORDEV );
-		hmac.update( JSON.stringify( req.body ) );
+  try {
+    var hmac = crypto.createHmac( 'sha1', process.env.WHSECRET_RTAYLORDEV );
+    hmac.update( JSON.stringify( req.body ) );
 
-		var calcSignature = new Buffer( 'sha1=' + hmac.digest( 'hex' ) );
-		var servSignature = new Buffer( req.headers['x-hub-signature'] );
+    var calcSignature = new Buffer( 'sha1=' + hmac.digest( 'hex' ) );
+    var servSignature = new Buffer( req.headers['x-hub-signature'] );
 
-		console.log( 'got webhook for rtaylordev from %s', req.headers['x-real-ip'] );
+    console.log( 'got webhook for rtaylordev from %s', req.headers['x-real-ip'] );
 
-		if ( calcSignature.equal( servSignature ) ) {
-			var options = { env: process.env };
-			exec( 'cd /home/ryan/web/rtaylordev && git pull', options, function ( error, stdout, stderr ) {
-				if ( error == null )
-					res.status( 200 ).send( stdout + stderr );
-				else
-					res.status( 500 ).send( 'There was an issue pulling from the repository\nData was not updated' );
-			});
-		}
-		else
-			res.status( 401 ).send( 'Signature mismatch' );
-	}
-	catch ( exception ) {
-		res.status( 500 ).send( 'The action could not be completed' );
-	}
+    if ( calcSignature.equal( servSignature ) ) {
+      var options = { env: process.env };
+      exec( 'cd /home/ryan/web/rtaylordev && git pull', options, function ( error, stdout, stderr ) {
+        if ( error == null )
+          res.status( 200 ).send( stdout + stderr );
+        else
+          res.status( 500 ).send( 'There was an issue pulling from the repository\nData was not updated' );
+      });
+    }
+    else
+      res.status( 401 ).send( 'Signature mismatch' );
+  }
+  catch ( exception ) {
+    res.status( 500 ).send( 'The action could not be completed' );
+  }
 });
 
 app.listen( 3999 );
