da218934c0441a3bc1b2b157e81657f285a9f83d
ryantaylor <ryan.taylor9@gmail.com>
2015-07-24 14:06:47 -0400

    Added GitHub hook signature calculations.

--- node/server.js
+++ node/server.js
@@ -1,1 +1,31 @@
-# nodejs server
+// nodejs server
+
+require( 'buffer-equal-constant-time' ).install();
+
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
+var crypto = require( 'crypto' );
+
+var app = express();
+
+app.use( bodyParser.json() );
+
+app.post( '/webhooks/rtaylordev', function ( req, res ) {
+	var hmac = crypto.createHmac( 'sha1', process.env.WHSECRET_RTAYLORDEV );
+	hmac.update( JSON.stringify( req.body ) );
+
+	var calcSignature = new Buffer( 'sha1=' + hmac.digest( 'hex' ) );
+	var servSignature = new Buffer( req.headers['x-hub-signature'] );
+
+	console.log( 'got webhook for rtaylordev from %s', req.headers['x-real-ip'] );
+
+	if ( calcSignature.equals( servSignature ) )
+		console.log( 'signatures match' );
+	else
+		console.log( 'signature mismatch error!' );
+
+	res.status( 200 ).send( 'OK' );
+});
+
+app.listen( 3999 );
+console.log( 'Listening on port 3999' );
