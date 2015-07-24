f709cff5c7b95445fdd33eed1b980063ca1ae603
ryantaylor <ryan.taylor9@gmail.com>
2015-07-24 14:59:13 -0400

    Added git pull exec to node server on GitHub webhook.

--- node/server.js
+++ node/server.js
@@ -3,6 +3,7 @@
 require( 'buffer-equal-constant-time' ).install();
 
 var express = require( 'express' );
+var exec = require( 'child_process' ).exec;
 var bodyParser = require( 'body-parser' );
 var crypto = require( 'crypto' );
 
@@ -19,8 +20,14 @@ app.post( '/webhooks/rtaylordev', function ( req, res ) {
 
 	console.log( 'got webhook for rtaylordev from %s', req.headers['x-real-ip'] );
 
-	if ( calcSignature.equal( servSignature ) )
-		console.log( 'signatures match' );
+	if ( calcSignature.equal( servSignature ) ) {
+		var options = { env: process.env };
+		exec( 'cd /home/ryan/web/rtaylordev && git pull', options, function ( error, stdout, stderr ) ) {
+			console.log( 'stdout: ' + stdout );
+			console.log( 'stderr: ' + stderr );
+			console.log( 'error: ' + error );
+		});
+	}
 	else
 		console.log( 'signature mismatch error!' );
 
